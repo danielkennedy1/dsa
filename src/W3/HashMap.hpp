f726a161dcfdb9b1cb8a72d1f7c8abe8ddf3cdfc
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-10-17 19:10:24 +0100

    HashMap seperate chaining without rehash

--- src/W3/HashMap.hpp
+++ src/W3/HashMap.hpp
@@ -1,63 +1,47 @@
 #include <iostream>
-#include <ostream>
-#include <string>
 
 template <typename K, typename V> struct MapNode {
     K key;
     V value;
     MapNode<K, V> *next;
-    MapNode<K, V>(K key, V value) : key(key), value(value), next(nullptr){};
+    MapNode<K, V>(K key, V value) : key(key), value(value), next(nullptr) {};
 };
 
 template <typename K, typename V> class HashMap {
 public:
-    HashMap<K, V>() { 
-        table = new MapNode<K, V> *[size]; 
-        for (int i = 0; i < size; i++) table[i] = nullptr;
-    }
-
-    void rehash(unsigned int newSize) {
-        MapNode<K, V> **newArray = new MapNode<K, V> *[newSize];
-
-        for (int i = 0; i < size; i++) {
-            if (table[i] != nullptr) {
-                MapNode<K, V> *node = table[i];
-                while (node != nullptr) {
-                    put(node->key, node->value, newArray, newSize);
-                    node = node->next;
-                }
-            }
-        }
-        delete[] table;
-        table = newArray;
-        size = newSize;
+    HashMap<K, V>() {
+        table = new MapNode<K, V> *[size];
+        for (int i = 0; i < size; i++)
+            table[i] = nullptr;
     }
 
     int size = 4; // size of array
     MapNode<K, V> **table;
-    // int count = 0; // number of elements
-    // float loadFactorTolerance = 0.5f;
+    int count = 0; // number of elements
+    double loadFactorTolerance = 0.5f;
 
-    void put(K key, V value, MapNode<K, V> **table, unsigned int arraySize) {
-        unsigned int location = hash(key, arraySize);
-        MapNode<K, V> *node = table[location];
+    void put(K key, V value) {
+        unsigned int hashValue = hash(key);
+        MapNode<K, V> *prev = nullptr;
+        MapNode<K, V> *node = table[hashValue];
 
-        if (node == nullptr) {
-            table[location] = new MapNode<K, V>(key, value);
-            return;
-        }
-
-        while (node->key != key) {
-            if (node->next == nullptr) {
-                node->next = new MapNode<K, V>(key, value);
-            }
+        while (node != nullptr && node->key != key) {
+            prev = node;
             node = node->next;
         }
 
-        // count++;
+        if (node == nullptr) {
+            MapNode<K, V> *entry = new MapNode<K, V>(key, value);
+            count++;
+            if (prev == nullptr) {
+                table[hashValue] = entry;
+            } else {
+                prev->next = entry;
+            }
+        } else {
+            node->value = value;
+        }
     }
-
-    void put(K key, V value) { put(key, value, table, size); }
 
     bool lookup(K key, V &output) {
         unsigned int location = hash(key);
@@ -74,14 +58,12 @@ public:
         return false;
     }
 
-    unsigned int hash(K &key) { return hash(key, size); }
-
-    unsigned int hash(K key, unsigned int arraySize) {
-        const char *s = reinterpret_cast<const char*>(&key);
+    unsigned int hash(K key) {
+        const char *s = reinterpret_cast<const char *>(&key);
         unsigned int result = 0;
         for (int i = 0; i < sizeof(key); ++i) {
             result = result * 101 + s[i];
         }
-        return result % arraySize;
+        return result % size;
     };
 };
--- test/W3/hashmap.cpp
+++ test/W3/hashmap.cpp
@@ -2,15 +2,40 @@
 #include <iostream>
 
 
+void printTable(HashMap<int, int>* hashMap) {
+    for (int i = 0; i < hashMap->size; i++) {
+        std::cout << "Index: " << i << " ";
+        if (hashMap->table[i] != nullptr) {
+            std::cout << hashMap->table[i]->key << std::endl;
+        } else {
+            std::cout << "NULL" << std::endl;
+        }
+    }
+    std::cout << std::endl;
+}
+
 int main() {
-    HashMap<const char*, int>* hashMap = new HashMap<const char*, int>();
+    HashMap<int, int>* hashMap = new HashMap<int, int>();
 
-    std::string key = "a123";
 
-    hashMap->put(key.c_str(), 22340017);
+    hashMap->put(1, 1);
+    printTable(hashMap);
+    hashMap->put(2, 2);
+    printTable(hashMap);
+    hashMap->put(3, 3);
+    printTable(hashMap);
+    hashMap->put(4, 4);
+    printTable(hashMap);
+    hashMap->put(5, 5);
+    printTable(hashMap);
 
     int v;
-    hashMap->lookup(key.c_str(), v); 
+    hashMap->lookup(5, v); 
 
-    std::cout << v << std::endl;
+    for (int i = 1; i < 6; i++) {
+        hashMap->lookup(i, v);
+        std::cout << v << std::endl;
+    }
+
+    std::cout << "Size: " << hashMap->size << std::endl;
 }
